61c6c113e2eae6f2f888e25787b919176ff95902
Mudasir Ali Nizamani <mudasiralinizamani@gmail.com>
2021-08-03 02:08:44 +0500

    Added PrimeNg Successfully in App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// PrimeNgModules
+import {RippleModule} from 'primeng/ripple';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
